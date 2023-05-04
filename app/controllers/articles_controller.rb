d402d1b3888b463ec45c6721549dbff2feeef8d1
Wendina <wendina.teng@gmail.com>
2023-05-04 17:28:14 +0000

    change index to show <h1> so hello rails displays on its route. also, change root route to show the same articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
+# The index action is empty. When an action does not explicitly render a view (or otherwise trigger an HTTP response), 
+# Rails will automatically render a view that matches the name of the controller and action.
+# So the index action will render app/views/articles/index.html.erb by default.
 class ArticlesController < ApplicationController
   def index
   end
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+
   get "/articles", to: "articles#index"
   ## route declares that GET /articles requests are mapped to the index action of ArticlesController
   ## to create ArticlesController and its index action, run the controller generator (with --skip-routes because we already have a route) in terminal
