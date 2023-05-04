4e1f5ef43188ca1dc6ef33d45ac202d177560918
Wendina <wendina.teng@gmail.com>
2023-05-04 16:44:18 +0000

    initial commit after installing dependencies. Also, generate controller Articles and add route for Articles.

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
+  ## route declares that GET /articles requests are mapped to the index action of ArticlesController
+  ## to create ArticlesController and its index action, run the controller generator (with --skip-routes because we already have a route) in terminal
+  ## bin/rails generate controller Articles index --skip-routes
+
+end
