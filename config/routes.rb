cd8fd271b9da193645db87d5f4be9d775e991fc7
Wendina <wendina.teng@gmail.com>
2023-05-04 22:34:37 +0000

    modify route to use the resources route method and clean up the index.html

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "articles#index"
 
-  get "/articles", to: "articles#index"
   ## route declares that GET /articles requests are mapped to the index action of ArticlesController
   ## to create ArticlesController and its index action, run the controller generator (with --skip-routes because we already have a route) in terminal
   ## bin/rails generate controller Articles index --skip-routes
+  
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  ## replace the two get routes using rails route method called resources (it maps all the conventional routes for a collection of resources)
 
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 
 end
