2cb96ac5d75de275ded0b18bcf2802281069404e
Wendina <wendina.teng@gmail.com>
2023-05-05 02:02:38 +0000

    fix spelling and syntax errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,10 +45,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:idi])
+    @article = Article.find(params[:id])
     @article.destroy
-    
-    redirect_to_root_path
+
+    redirect_to root_path
   end
 
   private
